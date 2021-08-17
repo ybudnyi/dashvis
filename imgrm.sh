fc12158985cb4611d9dc369dc7ecd931291dd45f
ybudnyi <77672787+ybudnyi@users.noreply.github.com>
2021-08-17 15:30:52 +0300

    Update imgrm.sh

--- imgrm.sh
+++ imgrm.sh
@@ -3,5 +3,5 @@ docker-compose stop
 docker-compose rm -f
 docker volume rm $(docker volume ls -q)
 #docker network rm $(docker network ls -q)
-docker image rm $(docker image ls -q)
+docker image rm $(docker image ls -q) -f
 echo "Cleaned workspace"
